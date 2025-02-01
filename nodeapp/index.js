8085259b97bbbbdcf4fef90a901aca621b5919e8
Nipun Prijanjith <133010944+nipun-priyanjith@users.noreply.github.com>
2025-02-01 21:09:15 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send(' it is work');
+  res.send(' helloww');
 });
 
 app.get("/will", function (req, res) {
